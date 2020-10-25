e9fed0fa27f8d715dedce7834f4f2312b977bb56
Eric Lake <ericlake@gmail.com>
2020-10-25 00:03:45 -0500

    Lots of updates to org mode related bits

--- org-config.el
+++ org-config.el
@@ -2,9 +2,13 @@
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
-(setq org-directory "~/Dropbox/org/"
-      org-use-speed-commands t
-      org-log-done-with-time t)
+(after! org
+  (setq org-directory "~/Dropbox/org"
+        org-enforce-todo-checkbox-dependencies nil
+        org-enforce-todo-dependencies t
+        org-habit-show-habits t
+        org-log-done-with-time t
+        org-use-speed-commands t))
 
 (custom-declare-face '+org-todo-active  '((t (:inherit (bold font-lock-constant-face org-todo)))) "")
 (custom-declare-face '+org-todo-project '((t (:inherit (bold font-lock-doc-face org-todo)))) "")
@@ -26,29 +30,40 @@
            "CANCLED(c)" ; Task was cancelled, aborted or is no longer applicable
            "DELEGATED(D)")) ; Task was delegated to someone else
         org-todo-keyword-faces
-        '(("WAIT" . +org-todo-onhold)
+        '(("TODO" . org-warning)
           ("PROJECT" . +org-todo-project)
-          ("TODO" . +org-todo-active)
-          ("NEXT" . +org-todo-next))))
+          ("IN_PROGRESS" . +org-todo-active)
+          ("NEXT" . +org-todo-next)
+          ("WAIT" . +org-todo-onhold)
+          ("BLOCKED" . +org-todo-onhold)
+          ("PROJECT" . (:foreground "blue" :weight bold))
+          ("CANCELED" . (:foreground "red" :weight bold)))))
 
 (after! org
   (setq org-capture-templates
-        '(("t" "Personal todo" entry
+        '(("p" "Personal Templates")
+          ("ph" "Personal habit" entry
+           (file+headline "~/Dropbox/org/habits.org" "Habit Tracker")
+           "\n** TODO %?\n%i\n"
+           :prepend nil
+           :unnarrowed t
+           :kill-buffer t)
+          ("pt" "Personal todo" entry
            (file+headline "~/Dropbox/org/personal-todo.org" "Personal Todo List")
-           "** TODO %?\n%i\n"
+           "\n** TODO %?\n%i\n"
            :prepend nil
            :unnarrowed t
            :kill-buffer t)
           ("w" "Work templates")
           ("wt" "Work todo" entry
           (file+headline "~/Dropbox/org/work-todo.org" "Work Todo List")
-          "** TODO %?\n%i\n"
+          "\n** TODO %?\n%i\n"
           :prepend nil
           :unnarrowed t
           :kill-buffer t)
           ("wl" "Work todo with link" entry
           (file+headline "~/Dropbox/org/work-todo.org" "Work Todo List")
-          "** TODO %?\n%i\n%a"
+          "\n** TODO %?\n%i\n%a"
           :prepend nil
           :unnarrowed t
           :kill-buffer t))))
@@ -70,17 +85,24 @@
    deft-strip-summary-regexp ".*$"
 )
 
+(after! org
+  (setq org-agenda-files (append (file-expand-wildcards "~/Dropbox/org/*.org"))))
+
 (use-package! org-super-agenda
   :after org-agenda
   :init
   (setq org-agenda-skip-scheduled-if-done t
-      org-agenda-skip-deadline-if-done t
-      org-agenda-include-deadlines t
-      ;; org-agenda-block-separator nil
-      ;; org-agenda-compact-blocks nil
-      org-agenda-start-day nil ;; i.e. today
-      ; org-agenda-span 10
-      org-agenda-start-on-weekday nil)
+        org-agenda-include-deadlines t
+        org-agenda-dim-blocked-tasks t
+        org-agenda-use-time-grid t
+        org-agenda-compact-blocks nil
+        org-agenda-skip-scheduled-if-done t
+        org-agenda-skip-deadline-if-done t
+        org-agenda-window-setup 'current-window
+        ;; org-agenda-block-separator nil
+        org-agenda-start-day nil ;; i.e. today
+        org-agenda-span 3
+        org-agenda-start-on-weekday nil)
   (setq org-super-agenda-groups
         '((:name "Today"
            :time-grid t
